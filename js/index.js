19e1f3174c67ebb1c9f05bc2cc2aac9fde3ba700
Htet-Wai-Yan <h.waiyan101@gmail.com>
2021-12-29 22:55:32 +0630

    Set transition default date to current date

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
 // Global variables
+let totalBudget = 0
+let totalExpense = 0
+
 let description = document.querySelector('#description')
 let balance = document.querySelector('#balance')
 let transitionDate = document.querySelector('#transitionDate')
 
-let totalBudget = 0
-let totalExpense = 0
+// set default date to current date
+let currentDate = new Date()
+currentDate = `${currentDate.getFullYear()}-${currentDate.getMonth()+1}-${currentDate.getDate()}`
 
-const transitionBtn = document.querySelector('#transitionBtn')
+transitionDate.value = currentDate
 
-transitionBtn.addEventListener('click', () => {
+const addTransition = document.querySelector('.addTransition')
+
+addTransition.addEventListener('submit', (e) => {
+
+  // disable page refresh on submit
+  e.preventDefault()
 
   // when both input fields are not empty
   if (budget.value !== "" && expense.value !== "") {
@@ -19,7 +28,7 @@ transitionBtn.addEventListener('click', () => {
   }
 
   // when one of them is empty
-  else if (budget.value !== "" || expense.value !== "") {
+  else if (budget.value !== "" || expense.value !== ""){
 
     // when expense is empty and vice versa
     if (expense.value !== "" || expense.value.length > 0 && budget.value == "") {
@@ -31,13 +40,11 @@ transitionBtn.addEventListener('click', () => {
 
   }
 
-  // when both are left empty
   else {
-    alert ('Field cannot be left blank')
+    alert ('Fields cannot be left blank')
   }
 
-  // reset the date picker
-  transitionDate.value = null
+  transitionDate.value = currentDate
 
 })
 
@@ -45,10 +52,11 @@ function calculateBudget(description = 'Budget added') {
   let budget = document.querySelector('#budget')
   let budgetAmount = document.querySelector('#budgetAmount')
 
-  totalBudget = parseInt(budget.value) + parseInt(budgetAmount.textContent)
+  totalBudget = parseFloat(budget.value) + parseFloat(budgetAmount.textContent)
+
   budgetAmount.textContent = totalBudget.toFixed(2)
 
-  balance.textContent = (parseInt(totalBudget) - parseInt(expenseAmount.textContent)).toFixed(2)
+  balance.textContent = (parseFloat(totalBudget) - parseFloat(expenseAmount.textContent)).toFixed(2)
 
   // History
   let transitionHistory = document.querySelector('#transitionHistory')
@@ -57,7 +65,7 @@ function calculateBudget(description = 'Budget added') {
   transitionHistory.appendChild(li).innerHTML = `
     <p>${description}</P>
     <div>
-      <span>${parseInt(budget.value).toFixed(2)}</span>
+      <span>${parseFloat(budget.value).toFixed(2)}</span>
       <span>${new Date(transitionDate.value).toLocaleDateString('en-GB')}</span>
     </div>
   `
@@ -76,15 +84,15 @@ function calculateExpense() {
   let transitionHistory = document.querySelector('#transitionHistory')
   let li = document.createElement('li')
 
-  totalExpense = parseInt(expense.value) + parseInt(expenseAmount.textContent)
+  totalExpense = parseFloat(expense.value) + parseFloat(expenseAmount.textContent)
   expenseAmount.textContent = totalExpense.toFixed(2)
 
-  balance.textContent = (parseInt(totalBudget) - parseInt(expenseAmount.textContent)).toFixed(2)
+  balance.textContent = (parseFloat(totalBudget) - parseFloat(expenseAmount.textContent)).toFixed(2)
 
   transitionHistory.appendChild(li).innerHTML = `
     <p>${description.value}</P>
     <div>
-      <span>${parseInt(expense.value).toFixed(2)}</span>
+      <span>${parseFloat(expense.value).toFixed(2)}</span>
       <span>${new Date(transitionDate.value).toLocaleDateString('en-GB')}</span>
     </div>
   `
