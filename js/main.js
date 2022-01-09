00f7b256177027ac52f98bae4a9fbe5337c0a750
Htet-Wai-Yan <h.waiyan101@gmail.com>
2022-01-09 22:53:16 +0630

    Start from scratch

---
+++ js/main.js
@@ -0,0 +1,96 @@
+// Model: Transition object
+class Transition {
+
+  constructor (_date, _description, _amount, _category) {
+    this.date = new Date(_date).toLocaleDateString('en-GB') // convert to dd/mm/yyyy
+    this.description = _description
+    this.amount = _amount
+    this.category = _category == "1" ? "Income" : "Expense"
+  }
+}
+
+// View: Display the data in the table rows
+class UI {
+
+  static displayTransition() {
+
+    // TO-DO: get this from localStorage
+    const storedTransition = []
+    
+    storedTransition.forEach(transition => UI.addTransition(transition))
+
+  }
+
+  static addTransition(newTransition) {
+
+    const tableData = document.querySelector('#tableData')
+    const tr = document.createElement('tr')
+    tr.innerHTML = `
+      <td class="align-middle">${newTransition.date}</td>
+      <td class="align-middle">${newTransition.description}</td>
+      <td class="align-middle">${newTransition.amount} MMK</td>
+      <td class="align-middle">${newTransition.category}</td>
+      <td class="align-middle">
+        <button class="btn btn-secondary deleteBtn">
+          <i class="fas fa-minus-circle text-danger"></i>
+        </button>
+      </td>
+    `
+
+    tableData.appendChild(tr)
+  }
+
+  static removeTransition(eleTarget) {
+    if(eleTarget.classList.contains('deleteBtn')) {
+      eleTarget.parentElement.parentElement.remove()
+    }
+  }
+
+  static showReminder() {
+    let alert = document.querySelector('.alert')
+
+    alert.classList.remove('d-none')
+
+    setTimeout(() => alert.classList.add('d-none'),2000)
+  }
+
+  static clearField() {
+    document.querySelector('#date').value = ""
+    document.querySelector('#description').value =""
+    document.querySelector('#amount').value = ""
+    document.querySelector('.form-select').value = "Choose one"
+  }
+}
+
+// Controller: display all items
+document.addEventListener('DOMContentLoaded', UI.displayTransition)
+
+// Controller: Add new item
+document.querySelector('#new_transition').addEventListener('submit', (event) => {
+
+  // disable form refresh behaviour
+  event.preventDefault()
+
+  // get the data from input fields
+  let date = document.querySelector('#date').value
+  let description = document.querySelector('#description').value
+  let amount = parseInt(document.querySelector('#amount').value).toFixed(2) // convert to 2 decimal
+  let category = document.querySelector('.form-select').value
+
+  // validate the input fields
+  if(!date || !description || !amount || !category || category == "Choose one") {
+    UI.showReminder()
+  } else {
+    let newTransition = new Transition(date, description, amount, category)
+
+    UI.addTransition(newTransition)
+
+    UI.clearField()
+  }
+
+})
+
+// Controller: Remove items from the table
+document.querySelector('#tableData').addEventListener('click', (element) => {
+  UI.removeTransition(element.target)
+})
